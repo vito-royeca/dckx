ae95162fd1176b158ee1132b6d0e190810e37f2b
Vito Royeca <jovit.royeca@gmail.com>
2020-02-13 16:20:32 -0500

    Created the Xcode project.

---
+++ dckx/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dckx
+//
+//  Created by Vito Royeca on 2/13/20.
+//  Copyright © 2020 Vito Royeca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
