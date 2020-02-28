c80069fc60a394615cbd9ff19907e06d7423d456
Vito Royeca <vito.royeca@gmail.com>
2020-02-28 11:55:34 -0500

    Added fastlane

---
+++ dckx/View/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  dckx
+//
+//  Created by Vito Royeca on 2/28/20.
+//  Copyright © 2020 Vito Royeca. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ dckx/View/WhatIfView.swift
@@ -0,0 +1,21 @@
+//
+//  WhatIfView.swift
+//  dckx
+//
+//  Created by Vito Royeca on 2/28/20.
+//  Copyright © 2020 Vito Royeca. All rights reserved.
+//
+
+import SwiftUI
+
+struct WhatIfView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WhatIfView_Previews: PreviewProvider {
+    static var previews: some View {
+        WhatIfView()
+    }
+}
